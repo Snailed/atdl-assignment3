502eea9732c23d2d76f6713f6dee08b9c8b4ee6b
Rasmus Løvstad <rasmus.hag.lovstad@gmail.com>
2024-11-04 18:14:40 +0100

    latest changes

--- exploration/data_utils.py
+++ exploration/data_utils.py
@@ -3,16 +3,10 @@ import torch_geometric.transforms as T
 import torch
 
 def get_cora(nodes_per_class):
-    transform = T.Compose([
-        T.NormalizeFeatures(),
-        T.RandomLinkSplit(num_val=0.05, num_test=0.1, is_undirected=True,
-                        split_labels=True, add_negative_train_samples=False),
-    ])
     dataset = Planetoid(
         root='../dataset',
         name='Cora',
         split='random',
-        num_train_per_class=nodes_per_class,
-        transform=transform
+        num_train_per_class=nodes_per_class
     )
     return dataset, dataset[0]
